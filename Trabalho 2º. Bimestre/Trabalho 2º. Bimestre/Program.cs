07b429ae2d3ed1848f407b5ca00c58d861e87652
Matheus Oliveira <matheusasariel@hotmail.com>
2019-06-16 08:06:30 -0300

    Princip

---
+++ Trabalho 2º. Bimestre/Trabalho 2º. Bimestre/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_2º.Bimestre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int op;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1.Cadastrar Café simples ");
+                Console.WriteLine("2.Cadastrar Café especial");
+                Console.WriteLine("3.Mostar cafés por tipo: ");
+                Console.WriteLine("4.Mostrar todos os cafés : ");
+                Console.Write("Sua opção:");
+                op = int.Parse(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                      
+                        break;
+                    case 2:
+                        
+                        break;
+                    case 3:
+                        
+                        break;
+                    case 4:
+
+                        break;
+                }
+
+
+            } while (op != 5);
+            Console.ReadKey();
+        }
+    }
+}
