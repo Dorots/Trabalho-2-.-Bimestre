da2ff4acf922c21cd9673210e9c8ee66f8960399
Whalysson A. Aguiar <whalyssonaguiar@hotmail.com>
2019-06-16 19:28:40 -0300

    Atualização.

---
+++ Trabalho 2º. Bimestre/Trabalho 2º. Bimestre/Especial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_2º.Bimestre
+{
+    class Especial : Cafe
+    {
+        private List<Cafe> LS;
+
+        public Especial()
+        {
+            LS = new List<Cafe>(); 
+        }
+
+        override public void Gravar(Cafe coffee)
+        {
+            LS.Add(coffee);
+        }
+        public override List<Cafe> Mostra()
+        {
+            return LS;
+        }
+    }
+}
