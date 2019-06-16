bad39e2952af2ba5f5a5ca4d109f4a85b3ec1d48
Matheus Oliveira <matheusasariel@hotmail.com>
2019-06-16 09:26:05 -0300

    Café

---
+++ Trabalho 2º. Bimestre/Trabalho 2º. Bimestre/Café.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_2º.Bimestre
+{
+   abstract class Café
+    {
+
+        private char tipo;
+        private string nome;
+        private int defeitos;
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+    }
+}
